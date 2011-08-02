c8287a5e8af28b8f120d920823ca8918e32bf4e4
Tom Martin <tompoges@googlemail.com>
2011-08-02 22:31:38 +0100

    Added new photo with permission from author

--- mrt_site/mrt_app/bg_info.py
+++ mrt_site/mrt_app/bg_info.py
@@ -1,5 +1,7 @@
 from django.conf import settings
 
-bg_info = [ {"url": settings.STATIC_URL + "bg/bg_6.jpg", "credit_name": "paolostar", "credit_link": "http://www.flickr.com/photos/paolostar/2056648771/sizes/z/in/photostream/"},
+bg_info = [
+            {"url": settings.STATIC_URL + "bg/bg_4.jpg", "credit_name": "xjrlokix", "credit_link": "http://www.flickr.com/photos/xjrlokix/2542767294/sizes/o/in/photostream/"}, 
+            {"url": settings.STATIC_URL + "bg/bg_6.jpg", "credit_name": "paolostar", "credit_link": "http://www.flickr.com/photos/paolostar/2056648771/sizes/z/in/photostream/"},
             {"url": settings.STATIC_URL + "bg/bg_7.jpg", "credit_name": "paolostar", "credit_link": "http://www.flickr.com/photos/paolostar/851782826/sizes/z/in/photostream/"},
             {"url": settings.STATIC_URL + "bg/bg_8.jpg", "credit_name": "paolostar", "credit_link": "http://www.flickr.com/photos/paolostar/466304689/sizes/m/in/photostream/"}]
