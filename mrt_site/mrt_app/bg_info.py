6509318b3ebaa05c1efffbea41a5e27a98fa1bd7
Tom Martin <tompoges@googlemail.com>
2011-08-03 19:37:16 +0100

    More bg images from flickr (with photographers permissions)

--- mrt_site/mrt_app/bg_info.py
+++ mrt_site/mrt_app/bg_info.py
@@ -2,6 +2,9 @@ from django.conf import settings
 
 bg_info = [
             {"url": settings.STATIC_URL + "bg/bg_4.jpg", "credit_name": "xjrlokix", "credit_link": "http://www.flickr.com/photos/xjrlokix/2542767294/sizes/o/in/photostream/"}, 
+            {"url": settings.STATIC_URL + "bg/bg_5.jpg", "credit_name": "C. G. P. Grey", "credit_link": "http://www.flickr.com/photos/52890443@N02/4888924743/sizes/l/in/photostream/"},
             {"url": settings.STATIC_URL + "bg/bg_6.jpg", "credit_name": "paolostar", "credit_link": "http://www.flickr.com/photos/paolostar/2056648771/sizes/z/in/photostream/"},
             {"url": settings.STATIC_URL + "bg/bg_7.jpg", "credit_name": "paolostar", "credit_link": "http://www.flickr.com/photos/paolostar/851782826/sizes/z/in/photostream/"},
-            {"url": settings.STATIC_URL + "bg/bg_8.jpg", "credit_name": "paolostar", "credit_link": "http://www.flickr.com/photos/paolostar/466304689/sizes/m/in/photostream/"}]
+            {"url": settings.STATIC_URL + "bg/bg_8.jpg", "credit_name": "paolostar", "credit_link": "http://www.flickr.com/photos/paolostar/466304689/sizes/m/in/photostream/"},
+            {"url": settings.STATIC_URL + "bg/bg_9.jpg", "credit_name": "kwl", "credit_link": "http://www.flickr.com/photos/kwl/1071916629/sizes/l/in/photostream/"},
+            {"url": settings.STATIC_URL + "bg/bg_11.jpg", "credit_name": "genista", "credit_link": "http://www.flickr.com/photos/genista/4211318/sizes/l/in/photostream/"}]
