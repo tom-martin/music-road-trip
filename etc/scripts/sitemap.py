646d867e4123002544324e1ecc74989e10e4f22a
Tom Martin <tompoges@googlemail.com>
2011-10-03 00:10:16 +0100

    Encode unicode before quoting it

--- etc/scripts/sitemap.py
+++ etc/scripts/sitemap.py
@@ -6,6 +6,6 @@ db = c["music_tour"]
 result_cache = db["results_cache"]
 
 for result in result_cache.find():
-  print "http://musicroadtrip.com/musictour/" + urllib.quote(result["results"][0]["name"]) + "/to/" + urllib.quote(result["results"][-1]["name"])
-  print "http://musicroadtrip.com/musictour/" + urllib.quote(result["results"][-1]["name"]) + "/to/" + urllib.quote(result["results"][0]["name"])
+  print "http://musicroadtrip.com/musictour/" + urllib.quote(result["results"][0]["name"].encode("utf-8")) + "/to/" + urllib.quote(result["results"][-1]["name"].encode("utf-8"))
+  print "http://musicroadtrip.com/musictour/" + urllib.quote(result["results"][-1]["name"].encode("utf-8")) + "/to/" + urllib.quote(result["results"][0]["name"].encode("utf-8"))
 
